a089071c55764ffd5765d638f9a88a62d8ca786e
nadia <verdhanadia@yahoo.com>
2024-07-30 17:17:47 +0200

    merge function added

--- data_func.py
+++ data_func.py
@@ -28,8 +28,8 @@ def merge_files(df, type='VV'):
     grouped.loc[:, 'date'] = grouped['time'].dt.date
     grouped_type = grouped[grouped['filepath'].str.contains(type)]
     grouped_type = grouped_type.groupby('date').agg({'filepath': list})
-
     for _, row in grouped_type.iterrows():
         output_path = output_directory + '/' +type + '_' + str(row.name) + '_merged.tiff'
         command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]} {row.filepath[2]} {row.filepath[3]}"
         os.system(command)
+
