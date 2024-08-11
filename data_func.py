46a2617d70b049667a4bd518b0553a025ae6f4a8
nadia <verdhanadia@yahoo.com>
2024-08-11 10:06:09 +0200

    update

--- data_func.py
+++ data_func.py
@@ -20,7 +20,6 @@ import tifftools
 # to merge the files
 def merge_files(df, type='VV'):
     output_directory = './merged_tiffs'
-
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     grouped = df[df['filepath'].str.endswith('.tif')]
@@ -29,34 +28,36 @@ def merge_files(df, type='VV'):
     grouped_type = grouped[grouped['filepath'].str.contains(type)]
     grouped_type = grouped_type.groupby('date').agg({'filepath': list})
     for _, row in grouped_type.iterrows():
-        output_path = output_directory + '/' +type + '_' + str(row.name) + '_merged.tiff'
+        output_path = output_directory + '/' + type + '_' + str(row.name) + '_merged.tiff'
         command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]} {row.filepath[2]} {row.filepath[3]}"
         os.system(command)
 
 
-
 # to merge the files and split into train, validation and test
-def merge_files_and_split(df, type='VV'):
+def merge_files_and_split(df, type='VV', notmask=True):
     grouped = df[df['filepath'].str.endswith('.tif')]
     grouped.loc[:, 'date'] = grouped['time'].dt.date
-    grouped_type = grouped[grouped['filepath'].str.contains('VH')]
-    grouped_type = grouped_type.groupby('date').agg({'filepath': list})
-    for _, row in grouped_type.iterrows():
+    if notmask:
+        grouped = grouped[grouped['filepath'].str.contains(type)]
+    grouped = grouped.groupby('date').agg({'filepath': list})
+    for _, row in grouped.iterrows():
         if (_.month == 4) | (_.month == 7):
-            directory = 'test'
-            os.makedirs(directory, exist_ok=True)
-            output_path = directory + '/' + type + '_' + str(row.name) + '_merged.tiff'
-            command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]} {row.filepath[2]} {row.filepath[3]}"
-            os.system(command)
+            merge('test', type, row, notmask)
         elif (_.month == 5) | (_.month == 8):
-            directory = 'val'
-            os.makedirs(directory, exist_ok=True)
-            output_path = directory + '/' + type + '_' + str(row.name) + '_merged.tiff'
-            command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]} {row.filepath[2]} {row.filepath[3]}"
-            os.system(command)
+            merge('val', type, row, notmask)
         else:
-            directory = 'train'
-            os.makedirs(directory, exist_ok=True)
-            output_path = directory + '/' + type + '_' + str(row.name) + '_merged.tiff'
-            command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]} {row.filepath[2]} {row.filepath[3]}"
-            os.system(command)
+            merge('train', type, row, notmask)
+
+
+def merge(directory, type, row, notmask):
+    if notmask:
+        os.makedirs(directory, exist_ok=True)
+        output_path = directory + '/' + type + '_' + str(row.name) + '_merged.tiff'
+        command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]} {row.filepath[2]} {row.filepath[3]}"
+        os.system(command)
+    else:
+        directory = directory + "_mask"
+        os.makedirs( directory, exist_ok=True)
+        output_path = directory + '/' + "mask" + '_' + str(row.name) + '_merged.tiff'
+        command = f"gdal_merge.py -o {output_path} {row.filepath[0]} {row.filepath[1]}"
+        os.system(command)
