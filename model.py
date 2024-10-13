133bfa904fe4021ed82dda119b4ded95914ee226
nadia <verdhanadia@yahoo.com>
2024-10-13 14:55:24 +0200

    adapted model

--- model.py
+++ model.py
@@ -98,8 +98,8 @@ class UNet(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         x = self.up1(x5, x4)
-        x = self.up2(x4, x3)
-        x = self.up3(x3, x2)
-        x = self.up4(x2, x1)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
